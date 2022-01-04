a505d26a52603ca5ec38010f2542fb19f7672eec
Vadim <forsomniaversas3@gmail.com>
2022-01-04 14:09:31 +0500

    fix http

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -13,18 +13,22 @@ class Main extends React.Component {
 
     componentDidMount() {
         fetch(
-            `http://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=avengers`
+            `https://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=avengers`
         )
             .then((response) => response.json())
             .then((data) =>
                 this.setState({ movies: data.Search, loading: false })
-            );
+            )
+            .catch((err) => {
+                console.log(err);
+                this.setState({ loading: false });
+            });
     }
 
     searchMovies = (str, type = 'all') => {
         this.setState({ loading: true });
         fetch(
-            `http://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=${str}${
+            `https://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=${str}${
                 type !== 'all' ? `&type=${type}` : ''
             }`
         )
