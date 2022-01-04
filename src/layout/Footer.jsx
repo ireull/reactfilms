1e7e2b2937f740d2952cc32e4183875b12937141
Vadim <forsomniaversas3@gmail.com>
2022-01-04 13:55:00 +0500

    complete proj

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer(){
+    
+return  <footer class="page-footer green lighten-4">
+            <div class="footer-copyright">
+                <div class="container">
+                © {new Date().getFullYear()} Copyright Text
+                <a class="grey-text text-lighten-4 right" href="#!">Repo</a>
+                </div>
+            </div>
+         </footer>
+}
+
+export {Footer}
---
+++ src/layout/Header.jsx
@@ -0,0 +1,15 @@
+function Header(){
+	return <nav className="green darken-1">
+				<div class="nav-wrapper">
+					 <a href="!#" class="brand-logo">Movies</a>
+				 <ul id="nav-mobile" class="right hide-on-med-and-down">
+					<li><a href="!#">MyRepo</a></li>
+					
+				 </ul>
+				</div>
+			</nav>
+		
+
+}
+
+export {Header}
