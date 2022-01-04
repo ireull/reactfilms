7d1f4100a874bcddfebde909d5f0c73b26808da8
Vadim <forsomniaversas3@gmail.com>
2022-01-04 14:21:13 +0500

    fix err

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,15 +1,21 @@
-function Header(){
-	return <nav className="green darken-1">
-				<div class="nav-wrapper">
-					 <a href="!#" class="brand-logo">Movies</a>
-				 <ul id="nav-mobile" class="right hide-on-med-and-down">
-					<li><a href="!#">MyRepo</a></li>
-					
-				 </ul>
-				</div>
-			</nav>
-		
-
+function Header() {
+    return (
+        <nav className="green darken-1">
+            <div class="nav-wrapper">
+                <a
+                    href="https://ireull.github.io/reactfilms"
+                    class="brand-logo"
+                >
+                    Movies
+                </a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li>
+                        <a href="https://github.com/ireull">MyRepo</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
 
-export {Header}
+export { Header };
